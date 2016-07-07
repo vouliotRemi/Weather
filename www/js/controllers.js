26a76c8f789430fc3d1dd01d50d352af1c6354d0
Rémi Vouliot <vouliot.remi@gmail.com>
2016-07-07 12:02:09 +0200

    final commit

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,7 @@
 angular.module('weather')
 
 .controller('WeatherCtrl', function($scope, $http, OpenWeatherConfig) {
+    console.log('test');
     $scope.search = '';
     $scope.state = false;    
     $scope.weatherData = {
@@ -11,16 +12,18 @@ angular.module('weather')
     temp: ''
     };
 
-    $scope.loadWeather = function(search) {
-        var url = OpenWeatherConfig.searchUrl + search + OpenWeatherConfig.units + OpenWeatherConfig.appid;
-        $http.get(url).success(function(data) {
-            $scope.weatherData.icon = OpenWeatherConfig.imgUrl + data.weather[0].icon + '.png';
-            $scope.weatherData.main = data.weather[0].main;
-            $scope.weatherData.city = data.name;
-            $scope.weatherData.description = data.weather[0].description;
-            $scope.weatherData.temp = data.main.temp;
-            $scope.state = true;
-        });
+        $scope.loadWeather = function(search, $event) {
+        console.log(search);
+        if ($event.keyCode === 13) {
+            var url = OpenWeatherConfig.searchUrl + search + OpenWeatherConfig.units + OpenWeatherConfig.appid;
+            $http.get(url).success(function(data) {
+                $scope.weatherData.icon = OpenWeatherConfig.imgUrl + data.weather[0].icon + '.png';
+                $scope.weatherData.main = data.weather[0].main;
+                $scope.weatherData.city = data.name;
+                $scope.weatherData.description = data.weather[0].description;
+                $scope.weatherData.temp = data.main.temp;
+                $scope.state = true;
+            });
+        }
     };
-
 });
